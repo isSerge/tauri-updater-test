b497a0e703babca07f843120c1f269308f7b81ef
Serge Kovbasiuk <isserge@protonmail.com>
2022-06-29 14:26:46 +0300

    add update event listener

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,17 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
+    .setup(|app| {
+      app.listen_global("tauri://update-available".to_string(), move |msg| {
+        println!("New version available: {:?}", msg);
+      });
+      Ok(())
+    })
     .menu(tauri::Menu::os_default(&context.package_info().name))
     .run(context)
     .expect("error while running tauri application");
